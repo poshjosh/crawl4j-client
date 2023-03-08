dd150fdfe3a1df8a1ec66c38f4e1898aca90113b
chinomso <posh.bc@gmail.com>
2023-03-08 07:28:22 +0100

    Remove RobotstxtConfig impl

--- src/main/java/io/github/poshjosh/crawl4j/client/CrawlLauncher.java
+++ src/main/java/io/github/poshjosh/crawl4j/client/CrawlLauncher.java
@@ -1,12 +1,7 @@
 package io.github.poshjosh.crawl4j.client;
 
-import edu.uci.ics.crawler4j.crawler.CrawlController;
-import io.github.poshjosh.crawl4j.client.elections2023.presidential.Urls;
-import io.github.poshjosh.crawl4j.client.net.UserAgents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.*;
 
 public class CrawlLauncher {
 
@@ -15,51 +10,7 @@ public class CrawlLauncher {
     //public static final int [] stateIds = {5, 7, 8, 13, 16, 18, 22, 24, 29, 30, 33, 34}; //, 36, 37
     public static final int [] stateIds = {33};
 
-
     public static void main(String[] args) throws Exception {
-
-        CrawlService crawlService = new CrawlService();
-
-        UserAgents userAgents = new UserAgents();
-
-        List<CrawlController> controllers = new ArrayList<>(stateIds.length);
-        for (int i = 0; i < stateIds.length; i++) {
-            String seedUrl = Urls.BASE + Urls.PRESIDENTIAL_ID + "?state=" + stateIds[i];
-            Set<String> seedUrls = new HashSet<>();
-            seedUrls.add(seedUrl);
-            logger.info("Starting crawler[{}] with seed urls: {}", i, seedUrls);
-            controllers.add(crawlService.startNonBlocking(userAgents.any(seedUrl, false), seedUrls, 1));
-        }
-
-        Thread.sleep(Timings.TIMEOUT_MILLIS);
-
-        logger.info("Shutting down {} crawler controllers", controllers.size());
-        for (CrawlController controller : controllers) {
-            controller.shutdown();
-        }
-
-        logger.info("Waiting until finish");
-        for (CrawlController controller : controllers) {
-            onCompleted(controller.getCrawlersLocalData());
-            controller.waitUntilFinish();
-        }
-    }
-
-
-    private static void onCompleted(List<Object> crawlersLocalData) {
-        int page = 0;
-        int links = 0;
-        int filesDownloaded = 0;
-        int bytesDownloaded = 0;
-        for (Object localData : crawlersLocalData) {
-            CrawlStat stat = (CrawlStat) localData;
-            page += stat.getTotalProcessedPages();
-            links += stat.getTotalLinks();
-            filesDownloaded += stat.getTotalFilesDownloaded();
-            bytesDownloaded += stat.getTotalBytesDownloaded();
-        }
-        logger.info("Aggregated Statistics:\nPages: {}, links: {}, downloaded: {}, bytes: {}, average download size: {}",
-                page, links, filesDownloaded, bytesDownloaded,
-                filesDownloaded == 0 ? 0 : (bytesDownloaded / filesDownloaded));
+        new CrawlService().start(stateIds);
     }
 }
--- src/main/java/io/github/poshjosh/crawl4j/client/CrawlService.java
+++ src/main/java/io/github/poshjosh/crawl4j/client/CrawlService.java
@@ -10,13 +10,22 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 import io.github.poshjosh.crawl4j.client.elections2023.presidential.DownloadLinkTest;
 import io.github.poshjosh.crawl4j.client.elections2023.presidential.UrlTest;
 import io.github.poshjosh.crawl4j.client.elections2023.PathUtil;
+import io.github.poshjosh.crawl4j.client.elections2023.presidential.Urls;
+import io.github.poshjosh.crawl4j.client.net.UserAgents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 
 public class CrawlService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CrawlService.class);
 
     private final Predicate<String> followUrlTest;
 
@@ -24,9 +33,59 @@ public class CrawlService {
 
     public CrawlService() {
         followUrlTest = new UrlTest();
-
         downloadLinkTest = new DownloadLinkTest();
     }
+
+    public List<CrawlController> start(int [] stateIds) throws Exception {
+
+        CrawlService crawlService = new CrawlService();
+
+        UserAgents userAgents = new UserAgents();
+
+        List<CrawlController> controllers = new ArrayList<>(stateIds.length);
+        for (int i = 0; i < stateIds.length; i++) {
+            String seedUrl = Urls.BASE + Urls.PRESIDENTIAL_ID + "?state=" + stateIds[i];
+            Set<String> seedUrls = new HashSet<>();
+            seedUrls.add(seedUrl);
+            logger.info("Starting crawler[{}] with seed urls: {}", i, seedUrls);
+            controllers.add(crawlService.startNonBlocking(userAgents.any(seedUrl, false), seedUrls, 1));
+        }
+
+        if (Timings.TIMEOUT_MILLIS > -1) {
+            Thread.sleep(Timings.TIMEOUT_MILLIS);
+        }
+
+        logger.info("Shutting down {} crawler controllers", controllers.size());
+        for (CrawlController controller : controllers) {
+            controller.shutdown();
+        }
+
+        logger.info("Waiting until finish");
+        for (CrawlController controller : controllers) {
+            onCompleted(controller.getCrawlersLocalData());
+            controller.waitUntilFinish();
+        }
+
+        return controllers;
+    }
+
+    private void onCompleted(List<Object> crawlersLocalData) {
+        int page = 0;
+        int links = 0;
+        int filesDownloaded = 0;
+        int bytesDownloaded = 0;
+        for (Object localData : crawlersLocalData) {
+            CrawlStat stat = (CrawlStat) localData;
+            page += stat.getTotalProcessedPages();
+            links += stat.getTotalLinks();
+            filesDownloaded += stat.getTotalFilesDownloaded();
+            bytesDownloaded += stat.getTotalBytesDownloaded();
+        }
+        logger.info("Aggregated Statistics:\nPages: {}, links: {}, downloaded: {}, bytes: {}, average download size: {}",
+                page, links, filesDownloaded, bytesDownloaded,
+                filesDownloaded == 0 ? 0 : (bytesDownloaded / filesDownloaded));
+    }
+
 
     public CrawlController start(String userAgent, Set<String> seedUrls, int numberOfCrawlers) throws Exception{
 
@@ -67,7 +126,7 @@ public class CrawlService {
         // Instantiate the controller for this crawl.
         PageFetcher pageFetcher = new PageFetcherImpl(config, crawlStat, followUrlTest);
         Parser parser = new Parser(config);
-        RobotstxtConfig robotstxtConfig = new RobotstxtConfigImpl(userAgent);
+        RobotstxtConfig robotstxtConfig = createRobotsConfig(userAgent);
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
         CrawlController controller = new CrawlController(config, pageFetcher, parser, robotstxtServer);
         seedUrls.stream().forEach(controller::addSeed);
@@ -86,5 +145,12 @@ public class CrawlService {
         crawlConfig.setResumableCrawling(true);
         //c.setShutdownOnEmptyQueue(false); // We have our shutdown command
         crawlConfig.setUserAgentString(userAgent);
+    }
+
+    private RobotstxtConfig createRobotsConfig(String userAgent) {
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        robotstxtConfig.setUserAgentName(userAgent);
+        robotstxtConfig.setIgnoreUADiscrimination(true);
+        return robotstxtConfig;
     }
 }
